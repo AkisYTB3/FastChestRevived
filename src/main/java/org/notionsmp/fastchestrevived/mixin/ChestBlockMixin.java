22a8b452591559c2bb03ca7704634fb2249a4d5c
Richard Mikloš <akisytbsteamy@gmail.com>
2025-08-14 14:01:22 +0200

    fix: remove tag check from ChestBlockMixin

--- src/main/java/org/notionsmp/fastchestrevived/mixin/ChestBlockMixin.java
+++ src/main/java/org/notionsmp/fastchestrevived/mixin/ChestBlockMixin.java
@@ -10,7 +10,6 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import org.notionsmp.fastchestrevived.FastChestTags;
 import org.notionsmp.fastchestrevived.config.Config;
 
 @Mixin(ChestBlock.class)
@@ -19,7 +18,7 @@ public class ChestBlockMixin
     @Inject(method = "getTicker", at = @At("HEAD"), cancellable = true)
     private <T extends BlockEntity> void removeTicker(World world, BlockState state, BlockEntityType<T> type, CallbackInfoReturnable<BlockEntityTicker<T>> cir)
     {
-        if (Config.simplifiedChest && state.isIn(FastChestTags.COMPATIBLE_CHEST_BLOCKS))
+        if (Config.simplifiedChest)
         {
             cir.setReturnValue(null);
         }
